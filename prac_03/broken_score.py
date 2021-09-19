619bcccd4a53c08dd7f3d8d0bbcad6d33a4cc666
Jarred Muller <j.muller@live.com.au>
2021-09-19 20:06:25 +1000

    add functions to broken_score

--- prac_03/broken_score.py
+++ prac_03/broken_score.py
@@ -2,17 +2,33 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+import random
 
-score = float(input("Enter score: "))
+def main():
+    score = float(input("Enter score: "))
+    score_descriptor(score)
+    rand_score = random.randint(0, 100)
+    print(rand_score)
+    score_descriptor(rand_score)
 
-if score < 0 or score > 100:
-    print("Invalid score")
 
-elif score > 90:
-    print("Excellent")
 
-elif score >= 50:
-    print("Passable")
 
-else:
-    print("Bad")
+
+def score_descriptor(score):
+
+    if score < 0 or score > 100:
+        outcome = "Invalid score"
+
+    elif score > 90:
+        outcome = "Excellent"
+
+    elif score >= 50:
+        outcome = "Passable"
+
+    else:
+        outcome = "Bad"
+    print(outcome)
+
+
+main()
